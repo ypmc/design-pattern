a9d29004c5a0dfd43b63730a9ff3edb24e1229ff
ypmc <loukangkang@wanda.cn>
2018-03-05 16:12:49 +0800

    initial

--- src/main/java/adaptor/OPPO.java
+++ src/main/java/adaptor/OPPO.java
@@ -5,7 +5,7 @@ package adaptor;
  */
 public class OPPO {
     public void charing(TypeC typeC) {
-        System.out.println("I'm oppo, charging from computer usb interface: adaptor works as follows:");
-        System.out.println(String.format("TypeC output is %s, that is perfect for oppo", typeC.output()));
+        System.out.println("I'm oppo R9, charging from computer usb interface: adaptor works as follows:");
+        System.out.println(String.format("TypeC output is %s, that is perfect for oppo R9", typeC.output()));
     }
 }
